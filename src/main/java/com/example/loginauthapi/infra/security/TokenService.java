324e97e592c96a522314d87b2cb1cbec3baa9085
candinho <can_dinho7@hotmail.com>
2024-05-16 12:38:34 -0300

    Token Service

--- src/main/java/com/example/loginauthapi/infra/security/TokenService.java
+++ src/main/java/com/example/loginauthapi/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.example.loginauthapi.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.loginauthapi.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,19 @@ public class TokenService {
             return token;
         }catch (JWTCreationException e){
             throw new RuntimeException("Error while authenticating");
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            return null;
         }
     }
 
